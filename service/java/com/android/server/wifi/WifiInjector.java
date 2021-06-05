LineageOS/android_frameworks_opt_net_wifi

wifi: Add StaState API [2/2]
Change-Id: I1f51ae6189a909132f566ebddfb6f9cbdf9acb2d

service/java/com/android/server/wifi/WifiInjector.java
@@ -102,6 +102,7 @@ public class WifiInjector {
     private final ScoringParams mScoringParams;
     private final ClientModeImpl mClientModeImpl;
     private final ActiveModeWarden mActiveModeWarden;
+    private final WifiStaStateNotifier mWifiStaStateNotifier;
     private final WifiSettingsStore mSettingsStore;
     private OpenNetworkNotifier mOpenNetworkNotifier;
     private CarrierNetworkNotifier mCarrierNetworkNotifier;
@@ -346,6 +347,8 @@ public class WifiInjector {
                 mWifiConfigManager, mContext, mDppMetrics);
         mIpMemoryStore = IpMemoryStore.getMemoryStore(mContext);
 
+        mWifiStaStateNotifier = new WifiStaStateNotifier(clientModeImplLooper, this);
+
         // Register the various network evaluators with the network selector.
         mWifiNetworkSelector.registerNetworkEvaluator(mSavedNetworkEvaluator);
         mWifiNetworkSelector.registerNetworkEvaluator(mNetworkSuggestionEvaluator);
@@ -450,6 +453,10 @@ public class WifiInjector {
 
     public ActiveModeWarden getActiveModeWarden() {
         return mActiveModeWarden;
+    }
+
+    public WifiStaStateNotifier getWifiStaStateNotifier() {
+        return mWifiStaStateNotifier;
     }
 
     public WifiSettingsStore getWifiSettingsStore() {

AOSIP/platform_frameworks_opt_net_wifi

service/java/com/android/server/wifi/WifiInjector.java
@@ -106,6 +106,7 @@ public class WifiInjector {
     private final ScoringParams mScoringParams;
     private final ClientModeImpl mClientModeImpl;
     private final ActiveModeWarden mActiveModeWarden;
+    private final WifiStaStateNotifier mWifiStaStateNotifier;
     private final WifiSettingsStore mSettingsStore;
     private OpenNetworkNotifier mOpenNetworkNotifier;
     private final WifiLockManager mLockManager;
@@ -382,6 +383,8 @@ public class WifiInjector {
         mWifiNetworkSelector.registerNetworkNominator(mNetworkSuggestionNominator);
         mWifiNetworkSelector.registerNetworkNominator(mScoredNetworkNominator);
 
+        mWifiStaStateNotifier = new WifiStaStateNotifier(wifiLooper, this);
+
         mClientModeImpl.start();
     }
 
@@ -477,6 +480,10 @@ public class WifiInjector {
 
     public ActiveModeWarden getActiveModeWarden() {
         return mActiveModeWarden;
+    }
+
+    public WifiStaStateNotifier getWifiStaStateNotifier() {
+        return mWifiStaStateNotifier;
     }
 
     public WifiSettingsStore getWifiSettingsStore() {